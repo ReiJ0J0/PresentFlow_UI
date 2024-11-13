606466324a595e1bc098e4a75886c722c89d526f
MHombre <markreimanh@gmail.com>
2024-11-13 20:10:57 +0800

    pathing for modules

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 async function loadContent() {
     try {
         // Fetch content for each section
-        const topBar = await fetch('/PresentFlow_UI/topbar.html').then(res => res.text());
-        const sidebar = await fetch('/PresentFlow_UI/sidebar.html').then(res => res.text());
-        const mainContent = await fetch('/PresentFlow_UI/maincontent.html').then(res => res.text());
-        const aboutSystem = await fetch('/PresentFlow_UI/aboutsystem.html').then(res => res.text());
-        const aboutUs = await fetch('/PresentFlow_UI/aboutus.html').then(res => res.text());        
+        const topBar = await fetch('topbar.html').then(res => res.text());
+        const sidebar = await fetch('sidebar.html').then(res => res.text());
+        const mainContent = await fetch('maincontent.html').then(res => res.text());
+        const aboutSystem = await fetch('aboutsystem.html').then(res => res.text());
+        const aboutUs = await fetch('aboutus.html').then(res => res.text());        
 
         // Insert fetched content into respective elements
         document.getElementById('top-bar').innerHTML = topBar;
